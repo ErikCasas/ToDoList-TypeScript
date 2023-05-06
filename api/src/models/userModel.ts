c5df461819bfac2fe9ce7e0edc7b6a82e97b1e86
ErikCasas <santicasas667@gmail.com>
2023-05-05 23:03:10 -0500

    models

---
+++ api/src/models/userModel.ts
@@ -0,0 +1,28 @@
+import { Schema, model, Model } from 'mongoose';
+import { Role, User } from '../types';
+
+const userSchema = new Schema({
+  name: {
+    type: String,
+    require: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    require: true,
+    trim: true,
+  },
+  passwordHash: {
+    type: String,
+    require: true,
+    trim: true,
+  },
+  role: {
+    type: String as Role,
+    require: true,
+    trim: true,
+  }
+});
+const User: Model<User> = model<User>('UserNotesModel', userSchema);
+
+export default User;
---
+++ api/src/types.d.ts
@@ -0,0 +1,20 @@
+import { Document } from 'mongoose';
+
+export type State = 'To Do' | 'Doing' | 'Doed';
+export type Role<string> = 'Admin' | 'User' | 'Disabled';
+
+export interface Note extends Document {
+  description: string;
+  important: boolean;
+  tittle: string;
+  user: object;
+  state: State;
+}
+
+export interface User extends Document {
+  passwordHash: string;
+  email: string;
+  name: string;
+  notes: Array;
+  role: Role;
+}
